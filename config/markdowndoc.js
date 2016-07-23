f0f44299aad2f3742dcfe662d57c12fa2361999d
scott-wyatt <scottwyatt86@gmail.com>
2016-07-23 17:57:12 -0400

    add trailpack-markdown-doc

---
+++ config/markdowndoc.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+  // Directory containing docs
+  path: '/docs',
+  // Prefix to be used for routes
+  prefix: 'docs',
+  // The layout page to embed the doc
+  layout: 'index.ejs'
+}
